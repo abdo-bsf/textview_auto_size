819d20f783e2bb9989d4e62cd8003e84aabad19f
abdo_bsf <basafou.abderrahim.ios@gmail.com>
2020-08-05 11:29:46 +0100

    TextView auto size

---
+++ TextView/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TextView
+//
+//  Created by basafou on 8/5/20.
+//  Copyright © 2020 basafou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+    }
+
+
+}
+
